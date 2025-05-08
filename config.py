cc520ac5a5eed18c81f1579ec9290a0eaeca1e63
Muneeb Anjum <muneebanjum370@gmail.com>
2025-05-08 22:39:54 +0500

    Updated 1.0

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # config.py
 
 API_URL = "https://openrouter.ai/api/v1/chat/completions"
-API_KEY = "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"
+API_KEY = "YOUR API KEY HERE"
 MODEL_NAME = "deepseek/deepseek-chat:free"
